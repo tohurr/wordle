56df01e289de254b760f8f04ec8ab8b1fb1d5114
Tohur Rahman <tohurrahman@Tohurs-MBP.home>
2022-05-19 16:25:15 -0400

    Created a Key component to put logic inside of it.

---
+++ src/components/Key.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Key({ keyVal }) {
+  return <div className='key'>{keyVal}</div>
+  
+}
+
+export default Key
--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Key from './Key';
 
+//creating 3 lines since there are 3 rows on the keyboard
 function Keyboard() {
+  const keys1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
+  const keys2 = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
+  const keys3 = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+  
   return (
-    <div>Keyboard</div>
-  )
+  <div className = 'keyboard'>
+          
+          <div className = 'line1'> {keys1.map((key) => {return <Key keyVal={key}/>;} ) } </div>
+          
+          <div className = 'line2'> {keys2.map((key) => {return <Key keyVal={key}/>;} ) } </div>
+          
+          <div className = 'line3'> {keys3.map((key) => {return <Key keyVal={key}/>;} ) } </div>
+      </div>
+  );
+
 }
 
-export default Keyboard
+export default Keyboard;
