a4a59e2e8a4edca6943b2061146893aeccbc01f9
Tohur Rahman <tohurrahman@Tohurs-MBP.home>
2022-05-19 11:43:29 -0400

    added a 'Letter' component and finished setting up the board

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './css/App.css';
-//import './components/Keyboard';
+import WBoard from './components/WBoard';
+import Keyboard from './components/Keyboard';
+import { createContext, useState} from 'react'
+import { boardDefault } from './Words';
+
+export const AppContext = createContext();
 
 function App() {
+  const [board, setWBoard] = useState(boardDefault);
   return (
     <div className="App">
       <nav>
         <h1>Wordle</h1>
       </nav>
-      <wBoard />
-      <Keyboard />
       
+      <AppContext.Provider value= { board, setWBoard}>
+      <WBoard />
+      <Keyboard />
+      </AppContext.Provider>
       
     </div>
   );
--- src/components/wBoard.js
+++ src/components/wBoard.js
@@ -1,11 +1,68 @@
 import React, {useState} from 'react';
-import { boardDefault } from './components/Words';
+import Letter from './Letter'
 
-function wBoard() {
-    const [board, setwBoard] = useState(boardDefault);
+function WBoard() {
+    
   return (
-    <div>wBoard</div>
-  )
+  <div className = 'board'>
+
+      <div className = 'row'> 
+      
+      <Letter letterPos={0} attemptVal={0}/> 
+      <Letter letterPos={1} attemptVal={0}/>
+      <Letter letterPos={2} attemptVal={0}/>
+      <Letter letterPos={3} attemptVal={0}/>
+      <Letter letterPos={4} attemptVal={0}/>
+      </div>
+      
+      <div className = 'row'> 
+      
+      <Letter letterPos={0} attemptVal={1}/> 
+      <Letter letterPos={1} attemptVal={1}/>
+      <Letter letterPos={2} attemptVal={1}/>
+      <Letter letterPos={3} attemptVal={1}/>
+      <Letter letterPos={4} attemptVal={1}/>
+      
+      </div>
+      
+      <div className = 'row'> 
+      <Letter letterPos={0} attemptVal={2}/> 
+      <Letter letterPos={1} attemptVal={2}/>
+      <Letter letterPos={2} attemptVal={2}/>
+      <Letter letterPos={3} attemptVal={2}/>
+      <Letter letterPos={4} attemptVal={2}/>
+      
+      </div>
+      
+      <div className = 'row'> 
+      <Letter letterPos={0} attemptVal={3}/> 
+      <Letter letterPos={1} attemptVal={3}/>
+      <Letter letterPos={2} attemptVal={3}/>
+      <Letter letterPos={3} attemptVal={3}/>
+      <Letter letterPos={4} attemptVal={3}/>
+      
+      </div>
+      
+      <div className = 'row'> 
+      <Letter letterPos={0} attemptVal={4}/> 
+      <Letter letterPos={1} attemptVal={4}/>
+      <Letter letterPos={2} attemptVal={4}/>
+      <Letter letterPos={3} attemptVal={4}/>
+      <Letter letterPos={4} attemptVal={4}/>
+      
+      </div>
+      
+      <div className = 'row'> 
+      <Letter letterPos={0} attemptVal={5}/> 
+      <Letter letterPos={1} attemptVal={5}/>
+      <Letter letterPos={2} attemptVal={5}/>
+      <Letter letterPos={3} attemptVal={5}/>
+      <Letter letterPos={4} attemptVal={5}/>
+      
+      </div>
+    
+    </div>
+  );
 }
 
-export default wBoard
+export default WBoard
