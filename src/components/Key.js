d058651c2e67524ad91007f38a4cf5fd4f89853c
Tohur Rahman <tohurrahman@Tohurs-MBP.home>
2022-05-19 22:05:22 -0400

    Now if the user types on the keyboard they will be able to move forward to the next box on the board but will not be able to go to the next row yet.

--- src/components/Key.js
+++ src/components/Key.js
@@ -2,12 +2,14 @@ import React, { useContext } from 'react';
 import {AppContext} from '../App.js';
 
 function Key({ keyVal, bigKey }) {
-    const {board, setBoard, currAttempt} = useContext(AppContext);
+    const {board, setBoard, currAttempt, setCurrAttempt } = useContext(AppContext);
 
     const selectLetter = () => {
+        if (currAttempt.letterPos > 4) return;
         const currBoard = [...board]
         currBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
         setBoard(currBoard);
+        setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos + 1})
         
     }
 
