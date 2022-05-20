4ba31fa5b47497db1a7ed7f70bbd10e0dd0ce97d
Tohur Rahman <tohurrahman@Tohurs-MBP.home>
2022-05-20 18:58:54 -0400

    Created a function using the useCallback hook in order to detect physical keyboard events in React.

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Key from './Key';
 
 //creating 3 lines since there are 3 rows on the keyboard
@@ -6,9 +6,23 @@ function Keyboard() {
   const keys1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
   const keys2 = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
   const keys3 = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+
+  const handleKeyboard = useCallBack((event) => {
+    
+  })
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyboard);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyboard );
+    };
+    }, [handleKeyboard]);
+
+
   
   return (
-  <div className = 'keyboard'>
+  <div className = 'keyboard' onKeyDown = {handleKeyboard}>
           
           <div className = 'line1'> {keys1.map((key) => {return <Key keyVal={key}/>; } ) } </div>
           
