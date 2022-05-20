e0fce2c3874f8dc671f21e8449c2a2e449d60668
Tohur Rahman <tohurrahman@Tohurs-MBP.home>
2022-05-20 09:55:04 -0400

    Created logic for 'ENTER'. Now if the user types in 5 letters and then hits 'ENTER' they will be able to go to the next row.

--- src/components/Key.js
+++ src/components/Key.js
@@ -5,13 +5,22 @@ function Key({ keyVal, bigKey }) {
     const {board, setBoard, currAttempt, setCurrAttempt } = useContext(AppContext);
 
     const selectLetter = () => {
+
+        if (keyVal === 'ENTER'){
+
+         if (currAttempt.letterPos !==5) return; 
+           setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
+        
+        } else {
+
         if (currAttempt.letterPos > 4) return;
         const currBoard = [...board]
         currBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
         setBoard(currBoard);
-        setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos + 1})
+        setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos + 1});
+        }
         
-    }
+    };
 
   return (
   <div className='key' id={bigKey && 'big'} onClick={selectLetter}>
