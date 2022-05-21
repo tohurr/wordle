315601adcf08700d3c435df2febe20acddf54bfc
Tohur Rahman <tohurrahman@Tohurs-MBP.home>
2022-05-20 21:37:41 -0400

    It is now possible to hit the 'enter' key and the 'backspace' key on the physical keyboard to make those actions happen on to the web page.

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,15 +1,42 @@
-import React, {useEffect} from 'react';
+import React, {useContext, useCallback, useEffect} from 'react';
+import {AppContext} from '../App.js';
 import Key from './Key';
 
 //creating 3 lines since there are 3 rows on the keyboard
 function Keyboard() {
+
+  const { onEnter, onDelete, onSelectLetter } = useContext(AppContext);
+
   const keys1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
   const keys2 = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
   const keys3 = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
 
-  const handleKeyboard = useCallBack((event) => {
-    
-  })
+  const handleKeyboard = useCallback((event) => {
+    if (event.key === 'Enter') {
+      onEnter();
+
+    } else if (event.key === 'Backspace') {
+      onDelete();
+      
+    } else {
+      keys1.forEach((key) => {
+        if (event.key === key) {
+          onSelectLetter(key)
+        }
+      });
+      keys2.forEach((key) => {
+        if (event.key === key) {
+          onSelectLetter(key)
+        }
+      });
+      keys3.forEach((key) => {
+        if (event.key === key) {
+          onSelectLetter(key)
+        }
+      } );
+      
+    }
+  });
 
   useEffect(() => {
     document.addEventListener('keydown', handleKeyboard);
